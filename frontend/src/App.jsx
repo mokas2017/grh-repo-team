6702b12b473d4e12e7bbb23561f4ff17dc4e93a0
mokas2017 <32878472+mokas2017@users.noreply.github.com>
2025-12-18 10:23:45 +0100

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,41 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import AdminDashboard from "./pages/dasboard/AdminDashboard";
+import Unauthorized from "./pages/Unauthorized";
+import NavBare from "./components/NavBare";
+import Footer from "./components/Footer";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { current } from "../src/JS/features/authSlice";
+
+
+function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    //check if the user is logged in or not
+    dispatch(current());
+  }, [dispatch]);
+  return (
+    <>
+      <NavBare />
+      <Routes>
+        {/* //Routes pour l'affichage du form LOGIN */}
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+
+        {/* //Route pour tout ce qui est admin */}
+        <Route path="/admin/dashboard" element={<AdminDashboard />}>
+          <Route path="register" element={<Register />} />
+        </Route>
+
+        {/* //Route Sortie qand il n'est pas authoriser */}
+        <Route path="/unauthorized" element={<Unauthorized />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
